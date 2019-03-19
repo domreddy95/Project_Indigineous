e56ffbf972b5257d5aedbbf86c13253bc981130e
tinsycoder <173010022@iitb.ac.in>
2019-03-19 18:03:31 +0530

    added position and velocity as properties

--- particle.cpp
+++ particle.cpp
@@ -8,20 +8,36 @@ class particle
 private:
     long double charge;
     long double mass;
+    long double position[3];
+    long double velocity[3];
     std::string species_name;
     int species_identifier;
 public:
-    particle(long double arg1, long double mass, std::string species_name, int species_identifier);
+    particle();
+    particle(long double arg1, long double mass, long double position[3], long double velocity[3], std::string species_name, int species_identifier);
     ~particle();
     void particle::print_properties(particle);
+    std::vector <particle> particle::load_particles(double min_x,double max_x,double min_y,double max_y);
 };
 
-particle::particle(long double arg1 = 0, long double arg2 = 1, std::string arg3 = "unnamed", int arg4 = 0)
+particle::particle()
+{
+    charge = 0;
+    mass = 1;
+    position[0] = 0;    position[1] = 0;    position[2] = 0;
+    velocity[0] = 0;    velocity[1] = 0;    velocity[2] = 0;
+    species_name = "unnamed";
+    species_identifier = 0;
+}
+
+particle::particle(long double arg1, long double arg2,long double arg3[3], long double arg4[3], std::string arg5, int arg6)
 {
     charge = arg1;
     mass = arg2;
-    species_name = arg3;
-    species_identifier = arg4;
+    position[0] = arg3[0];    position[1] = arg3[1];    position[2] = arg3[2];
+    velocity[0] = arg4[0];    velocity[1] = arg4[1];    velocity[2] = arg4[2];
+    species_name = arg5;
+    species_identifier = arg6;
 }
 
 particle::~particle()
@@ -36,4 +52,9 @@ void particle::print_properties(particle arg1)
     cout << "mass = " << arg1.mass << endl;
     cout << "species_name = " << arg1.species_name << endl;
     cout << "species_identifier = " << arg1.species_identifier << endl;
+}
+
+std::vector <particle> particle::load_particles(double min_x,double max_x,double min_y,double max_y)
+{
+
 }
