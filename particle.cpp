cde1c43f92451719f68ef1286719fb7c709be5f5
tinsycoder <173010022@iitb.ac.in>
2019-03-24 06:11:29 +0530

    added load particles routine

--- particle.cpp
+++ particle.cpp
@@ -8,36 +8,38 @@ class particle
 private:
 
 public:
-    std::vector <long double> position;
-    std::vector <long double> velocity;
-    std::vector <long double> GetPosition(){return position;}
-    void SetPosition(std::vector <long double> position){position = position;}
-    std::vector <long double> GetVelocity(){return velocity;}
-    void SetVelocity(std::vector <long double> velocity){velocity = velocity;}
+    std::vector <double> position;
+    std::vector <double> velocity;
+    std::vector <double> GetPosition(){return position;}
+    void SetPosition(std::vector <double> position){position = position;}
+    std::vector <double> GetVelocity(){return velocity;}
+    void SetVelocity(std::vector <double> velocity){velocity = velocity;}
     particle();
-    particle(long double, long double);
+    particle(double, double);
     ~particle();  
 };
 
 //Define default Constructer
 particle::particle()
 {
-    std::vector <long double> position (2,0.0);
-    std::vector <long double> velocity (2,0.0);
-    std::cout << " A particle is created" << std::endl;
+    std::vector <double> position (2,0.0);
+    std::vector <double> velocity (2,0.0);
+    this->position = position;
+    this->velocity = velocity;
+    //std::cout << " A particle is created" << std::endl;
 }
 
 //Parameterized Constructor
-particle::particle(long double charge, long double mass)
+particle::particle(double charge, double mass)
 {
-    std::vector <long double> position (2,0.0);
-    std::vector <long double> velocity (2,0.0);
+    std::vector <double> position (2,0.0);
+    std::vector <double> velocity (2,0.0);
     std::cout << " A parametrized particle is created" << std::endl;
 }
 
 particle::~particle()
 {
-    std::cout << " A particle is destroyed";
+    //std::cout << " A particle is destroyed";
 }
 
 
@@ -47,32 +49,43 @@ class species: public particle
 private:
     std::string species_name;
     int species_identifier;
-    long double charge;
-    long double mass;
+    double charge;
+    double mass;
 
 public:
-    species(): particle(){};
-    species(std::string, int, long double, long double);
+    species();
+    species(std::string, int, double, double);
     ~species();
-    long double GetCharge(){return charge;}
-    void SetCharge(long double charge){this->charge=charge;}
-    long double GetMass(){return mass;}
-    void SetMass(long double mass){this->mass = mass;}
+    double GetCharge(){return charge;}
+    void SetCharge(double charge){this->charge=charge;}
+    double GetMass(){return mass;}
+    void SetMass(double mass){this->mass = mass;}
     void print_properties();
 };
 
-species::species(std::string species_name , int species_identifier, long double charge,
-long double mass) : particle()
+species::species() : particle()
+{
+    this->species_name = "no name";
+    this->species_identifier = 0;
+    this->charge = 0;
+    this->mass = 0;
+    //std::cout << " A species particle is created" << std::endl;
+};
+
+
+species::species(std::string species_name , int species_identifier, double charge,
+double mass) : particle()
 {
     this->species_name = species_name;
     this->species_identifier = species_identifier;
     this->charge = charge;
     this->mass = mass;
-    std::cout << " A parametrized species is created" << std::endl;
+    //std::cout << " A parametrized species is created" << std::endl;
 }
 
 species::~species()
 {
+    //std::cout << " A species particle is destroyed" << std::endl;
 }
 
 void species::print_properties()
@@ -81,16 +94,77 @@ void species::print_properties()
     using std::endl;
     cout << "charge = " << this->charge << endl;
     cout << "mass = " << this->mass << endl;
-    cout << "position_x = " << this->position[0]<<endl;
-    cout << "velocity_x = " << this->velocity[0]<<endl;
+    cout << "position_x = " << position[0] <<endl;
+    cout << "velocity_x = " << velocity[0] <<endl;
 }
+
+std::vector <species> create_species (double num_of_particles = 0)
+{
+    std::vector <species> temp_array;
+    temp_array.reserve(num_of_particles);
+    for (int i = 0; i<num_of_particles; i++ )
+    {
+        temp_array.emplace_back(species());
+    }
+    return temp_array;
+}
+
+std::vector <species> load_species (double min_dim1, double max_dim1, double min_dim2, double max_dim2, double density)
+{
+    double delta_dim1 = (max_dim1-min_dim1);
+    double delta_dim2 = (max_dim2-min_dim2);
+    int num_of_particles = delta_dim1*((max_dim2*max_dim2)-(min_dim2*min_dim2))*density;
+    int N = 1000;
+    double dy = (max_dim2-min_dim2)/N;
+    std::vector <species> temp_array1 = create_species(num_of_particles);
+    int counter1 = 0;
+    int counter2 = 0;
+    for (int i = 0; i<N; i++)
+    {
+        double r1 = (i*dy) + min_dim2;
+        double r2 = ((i+1)*dy) + min_dim2;
+        int num = ((r2*r2) - (r1*r1))*delta_dim1*density;
+        counter2 += num;
+        for (int j = counter1; j<counter2; j++)
+        {
+            temp_array1[j].position[0] =  min_dim1 + ((rand() / (RAND_MAX + 1.))*delta_dim1);
+            temp_array1[j].position[1] = r1 + ((rand() / (RAND_MAX + 1.))*dy);
+        }
+        counter1 = counter2;   
+    }
+    //std::cout << counter << std::endl;
+    if (counter2 < num_of_particles)
+    {
+        int num = num_of_particles - counter2;
+        counter2 += num;
+        for (int i = counter1; i < counter2; i++)
+        {
+            temp_array1[i].position[0] =  min_dim1 + ((rand() / (RAND_MAX + 1.))*delta_dim1);
+            temp_array1[i].position[1] = (0.70*delta_dim2) + ((rand() / (RAND_MAX + 1.))*(0.29*delta_dim2)); 
+        }
+    }
+    
+    return temp_array1;
+}
+
+
+
 int main()
 {
-    //long double chrg = 1.983742974237498;
-    //long double mas = 116.92847928349292;
-    species elec();
-    particle p;
-    p.position[0];
-    //ion.print_properties();
+    //double value;
+    std::vector <species> electrons;
+    //species electron;
+    //electron.print_properties();
+    //electron.SetCharge(0);
+    //electron.SetMass(0);
+    //electron.print_properties();
+    electrons = load_species(0,1,1,2,10000);
+    std::cout << electrons.size() << std::endl;
+    std::cout << electrons[0].position[1];
+    //value = electron.position[0];
+    //std::cout << value << std::endl;
+    //electron.position[0] = 1;
+    //value = electron.position[0];
+    //std::cout << value << std::endl;
     return 0;
 }
---
+++ test3.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+#include <vector> 
+
+using namespace std; 
+
+int main() 
+{ 
+	vector<int> g1 (5,1);
+    cout << "vector created" << endl;
+    cout << g1[0] << endl;
+
+
+
+}
